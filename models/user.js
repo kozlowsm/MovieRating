8fe8ffbcc739c223816c54f1b7da82a5965ce211
kozlowsm <kozlowsm@oregonstate.edu>
2018-08-30 21:07:42 -0400

    General User schema created for auth, may need more work

--- models/user.js
+++ models/user.js
@@ -1,1 +1,23 @@
 //User schema/model
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  googleID: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
