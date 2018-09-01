950000cac0114ae1e110a77e9d92e368d5c7cdb7
kozlowsm <kozlowsm@oregonstate.edu>
2018-09-01 14:20:47 -0400

    fixed search route

--- routes/movie.js
+++ routes/movie.js
@@ -9,31 +9,26 @@ const omdbURL = "https://www.omdbapi.com/?apikey=";
 router.get("/search", (req, res) => {
   //get the movie title from the user
   let movieTitle = req.query.movieTitle;
-  //res.render("movies/results", { movieTitle });
 
   //search api with user movie title
-  // axios <-- working code starts here
-  //   .get(`${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie`)
-  // Promise.all([
-  //   `${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie`,
-  //   `${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie&page=2`
-  // ])
-  //   .then(movies => {
-  //     //maybe flash a message and redirect back to search page/dashboard
-  //     //console.log(movies);
-  //     if (!movies.data.Response)
-  //       return res.status(400).send("No movies found.");
+  axios
+    .get(`${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie`)
+    .then(movies => {
+      //maybe flash a message and redirect back to search page/dashboard
+      //console.log(movies);
+      if (!movies.data.Response)
+        return res.status(400).send("No movies found.");
 
-  //     let movieData = movies.data;
-  //     console.log(movieData);
-  //     res.render("movies/results", { movieData });
-  //   })
-  //   .catch(err => {
-  //     res.status(400).send("Oops, something went wrong.");
-  //   });
+      let movieData = movies.data;
+      console.log(movieData);
+      res.render("movies/results", { movieData });
+    })
+    .catch(err => {
+      res.status(400).send("Oops, something went wrong.");
+    });
 });
 
-async function fetchMetaData(movieTitle) {
+function fetchMetaData(movieTitle) {
   let pagesRequired = 0;
   axios
     .get(`${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie`)
