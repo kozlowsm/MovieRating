953e868fa35aa1f31811f5e54a8370a36168dc09
kozlowsm <kozlowsm@oregonstate.edu>
2018-08-31 23:30:41 -0400

    added api keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID:
     "79970392106-cmp5g29erh1j08e2ps5q6olhg5qfe5pi.apps.googleusercontent.com",
-  googleClientSecret: "N9Ma5LRwppZruQk5KMR0jUvn"
+  googleClientSecret: "N9Ma5LRwppZruQk5KMR0jUvn",
+  omdbKey: "7d0edd58"
 };
