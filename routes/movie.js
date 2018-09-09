6c3763bf0efd3ab633ecbcabac62e50d0120cd77
kozlowsm <kozlowsm@oregonstate.edu>
2018-09-08 20:27:36 -0400

    Created post route for storing ratings and movies

--- routes/movie.js
+++ routes/movie.js
@@ -3,6 +3,9 @@ const axios = require("axios");
 const keys = require("./../config/keys");
 router = express.Router();
 
+const { Movie } = require("./../models/movie");
+const { Rating } = require("./../models/rating");
+
 //global var for link to access api
 const omdbURL = "https://www.omdbapi.com/?apikey=";
 
@@ -10,8 +13,16 @@ router.get("/search", (req, res) => {
   //get the movie title from the user
   let movieTitle = req.query.movieTitle;
 
+  let processed = [];
+
   //search api with user movie title
-  fetchPages(movieTitle);
+  /*  fetchPages(movieTitle)
+    .then(results => {
+      results.forEach(result => {
+        console.log(result.data);
+      });
+    })
+    .catch(err => console.log(err)); */
 
   axios
     .get(`${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie`)
@@ -22,7 +33,7 @@ router.get("/search", (req, res) => {
         return res.status(400).send("No movies found.");
 
       let movieData = movies.data;
-      console.log(movieData);
+      //console.log(movieData);
       res.render("movies/results", { movieData });
     })
     .catch(err => {
@@ -30,14 +41,28 @@ router.get("/search", (req, res) => {
     });
 });
 
+//show the specific movie and add its details to the database
 router.get("/show/:id", (req, res) => {
   let movieID = req.params.id;
   axios
-    .get(`${omdbURL}${keys.omdbKey}&i=${movieID}&plot=full`)
+    .get(`${omdbURL}${keys.omdbKey}&i=${movieID}`)
     .then(movie => {
       if (!movie.data.Response) return res.status(400).send("Invalid movie ID");
       let movieData = movie.data;
-      console.log(movieData);
+
+      let movieInfo = new Movie({
+        title: movie.data.Title,
+        imdbRating: movie.data.imdbRating,
+        runTime: movie.data.Runtime,
+        rating: movie.data.Rated,
+        poster: movie.data.Poster,
+        genre: movie.data.Genre,
+        actors: movie.data.Actors,
+        imdbID: movie.data.imdbID
+      });
+      //add the movie details to the collection
+      movieInfo.save();
+
       res.render("movies/show", { movieData });
     })
     .catch(err => {
@@ -45,29 +70,32 @@ router.get("/show/:id", (req, res) => {
     });
 });
 
+//Rating Post route - sends user rating to the database
+router.post("/rate/:id", (req, res) => {
+  let movieID = req.params.id;
+  //Get the rating info from the user
+  let userRating = req.body.rating; //add validation later!
+  let userComment = req.body.comment;
+
+  //add user data to rating collection
+  Movie.findOne({ imdbID: movieID })
+    .then(movieDB => {
+      let newRating = new Rating({
+        user: req.user._id,
+        rating: userRating,
+        comment: userComment,
+        movieID: movieDB._id
+      });
+      newRating.save().then(rating => {
+        res.redirect("/dashboard");
+      });
+    })
+    .catch(err => console.log(err));
+});
+
 //takes the title of the movie to search and the number of pages you want to return
 function fetchPages(movieTitle) {
   let pagesRequired = 0;
-  let omdbResponse = await axios.get(
-    `${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie`
-  );
-  pagesRequired = Math.ceil(omdbResponse.data.totalResults / 10);
-  const apiPromises = [];
-  if (pagesRequired > 1) {
-    for (let i = 0; i <= 2; i++) {
-      apiPromises.push(
-        axios.get(
-          `${omdbURL}${keys.omdbKey}&s=${movieTitle}&type=movie&page=${i}`
-        )
-      );
-    }
-    Promise.all(apiPromises)
-      .then(results => {
-        results.forEach(result => {
-          console.log(result);
-        });
-      });
-  }
 }
 
 function fetchMetaData(movieTitle) {
